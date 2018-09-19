650b02e101d41d22bcf44b9d4624c89ac4432d11
Alexey Lukashin <lukash@neva.ru>
2018-09-19 11:41:49 +0300

    First files from 3rd lecture

---
+++ src/ru/spbstu/tema/pp/HeyThere.java
@@ -0,0 +1,16 @@
+package ru.spbstu.tema.pp;
+
+import java.util.Vector;
+
+public class HeyThere {
+	public static void main(String[] args) {
+		
+		System.out.println("hey there!");
+		Vector<String> v = new Vector<>();
+		v.add("One");
+		v.add("Two");
+		
+		System.out.println(v);
+		
+	}
+}
